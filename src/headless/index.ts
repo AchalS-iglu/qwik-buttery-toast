80d3829866af629364ecdda601656131e44eb940
Achal Singhal <achals2002@gmail.com>
2023-08-24 01:17:44 +0530

    exports

---
+++ src/headless/index.ts
@@ -0,0 +1,18 @@
+import { toast } from '../core/toast';
+
+export type {
+  DefaultToastOptions,
+  IconTheme,
+  Renderable,
+  Toast,
+  ToasterProps,
+  ToastOptions,
+  ToastPosition,
+  ToastType,
+} from '../core/types';
+
+export { useToaster } from '../core/use-toaster';
+export { useStore as useToasterStore } from '../core/store';
+
+export { toast };
+export default toast;
